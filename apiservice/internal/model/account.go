4353cf5a902ee7bb3645084ff1ba78e73b38414a
YaeKitsune <aepnaonp@gmail.com>
2026-03-30 19:41:33 +0500

    Добавлена модель аккаунта

--- apiservice/internal/model/account.go
+++ apiservice/internal/model/account.go
@@ -1,1 +1,13 @@
 package model
+
+import "gorm.io/gorm"
+
+type Account struct {
+	gorm.Model
+	Surname    string `json:"surname" gorm:"column:surname"`
+	Name       string `json:"name" gorm:"column:name"`
+	Patronymic string `json:"patronymic" gorm:"column:patronymic"`
+	Role       string `json:"role"  gorm:"column:role;default:user"`
+	Login      string `json:"login" gorm:"column:login;unique"`
+	Password   string `json:"-" gorm:"column:password"`
+}
